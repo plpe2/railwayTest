8c38205c9e9927b3c43410f427fccdaf4a800c8d
plpe2 <147789736+plpe2@users.noreply.github.com>
2025-06-11 17:56:37 +0800

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-
+"use client"
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      Dashboard
-    </div>
-  );
+  useEffect(()=>{
+    const apiCall = async () =>{
+      const response = await fetch('http://localhost:5000/users')
+
+      const data = await response.json();
+
+      console.log(data)
+    }
+    apiCall();
+  }, [])
+  return <div>Dashboard</div>;
 }
