ee1885cd13c99bfdc8192776fcbf5cac9bb45b50
plpe2 <147789736+plpe2@users.noreply.github.com>
2025-06-12 00:03:55 +0800

    deploy in next API

---
+++ src/lib/db.ts
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: parseInt(process.env.MYSQLPORT || '3306')
+});
+
+export default connection;
