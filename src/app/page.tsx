59ca0670b57066902fc4f29beafac7cadd9f3dd6
plpe2 <147789736+plpe2@users.noreply.github.com>
2025-06-11 22:43:07 +0800

    fetching fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(()=>{
     const apiCall = async () =>{
-      const response = await fetch('/users')
+      const response = await fetch('http://localhost:8080/users')
 
       const data = await response.json();
 
